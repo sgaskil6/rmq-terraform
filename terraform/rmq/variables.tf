2c983825c508f0ec483a74af8888a537e9a06a14
sgaskil6 <helpinmyself2oo@gmail.com>
2022-02-09 04:52:53 -0500

    Terraform Jenkins fix

--- terraform/rmq/variables.tf
+++ terraform/rmq/variables.tf
@@ -1,11 +1,14 @@
 variable "name" {
     description = "Name the instance on deploy"
+    default = []
 }
 
 variable "group" {
     description = "The group tag for Ansible to identify RMQ"
+    default = []
 }
 
 variable "profile" {
     description = "Which profile to use for IAM"
+    default = []
 }
