d88da12629f3b579fd5490d35b7e8f0f651fc876
sgaskil6 <helpinmyself2oo@gmail.com>
2022-02-14 01:12:33 -0500

    remove Terraform default variables

--- terraform/rmq/variables.tf
+++ terraform/rmq/variables.tf
@@ -1,14 +1,11 @@
 variable "name" {
     description = "Name the instance on deploy"
-    default = "some"
 }
 
 variable "group" {
     description = "The group tag for Ansible to identify RMQ"
-    default = "someth"
 }
 
 variable "profile" {
     description = "Which profile to use for IAM"
-    default = "something"
 }
